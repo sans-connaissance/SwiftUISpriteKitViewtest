4640bb8f023af8679c99f7ed28035e3770830b39
David Malicke <davemalicke@gmail.com>
2024-09-24 22:08:10 -0400

    Initial Commit

---
+++ SwiftUISpriteKitViewtest/SwiftUISpriteKitViewtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISpriteKitViewtestApp.swift
+//  SwiftUISpriteKitViewtest
+//
+//  Created by David Malicke on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISpriteKitViewtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUISpriteKitViewtestTests/SwiftUISpriteKitViewtestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISpriteKitViewtestTests.swift
+//  SwiftUISpriteKitViewtestTests
+//
+//  Created by David Malicke on 9/24/24.
+//
+
+import Testing
+@testable import SwiftUISpriteKitViewtest
+
+struct SwiftUISpriteKitViewtestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
