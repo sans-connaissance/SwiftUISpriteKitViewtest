de827872384663fdf3486f800f3b9e8fdc14155b
David Malicke <davemalicke@gmail.com>
2024-10-05 17:28:36 -0400

    Add swiftui+extensions

---
+++ SwiftUISpriteKitViewtest/Extensions/SwiftUI+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftUI+Extensions.swift
+//  SwiftUISpriteKitViewtest
+//
+//  Created by David Malicke on 10/5/24.
+//
+
+import SwiftUI
+
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
--- SwiftUISpriteKitViewtest/Views/GameView.swift
+++ SwiftUISpriteKitViewtest/Views/GameView.swift
@@ -65,9 +65,3 @@ struct GameView: View {
         }
     }
 }
-
-extension UIScreen{
-   static let screenWidth = UIScreen.main.bounds.size.width
-   static let screenHeight = UIScreen.main.bounds.size.height
-   static let screenSize = UIScreen.main.bounds.size
-}
