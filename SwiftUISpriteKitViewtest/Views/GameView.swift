999e3b5d11f91d090bc5e68a40ccc612599e75b6
David Malicke <davemalicke@gmail.com>
2024-10-05 12:10:42 -0400

    Add dragGesture logic

--- SwiftUISpriteKitViewtest/Views/GameView.swift
+++ SwiftUISpriteKitViewtest/Views/GameView.swift
@@ -11,36 +11,57 @@ import GameplayKit
 
 struct GameView: View {
     @State private var vm = GameViewModel()
+    @State private var isLocked: Bool = true
     let levelTitle: String
     
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                
-                ForEach(vm.skScenes, id: \.self) { scene in
-                    SpriteView(
-                        scene: scene,
-                        isPaused: false,
-                        preferredFramesPerSecond: 120,
-                        options: [.ignoresSiblingOrder, .shouldCullNonVisibleNodes],
-                        debugOptions: [.showsFPS, .showsNodeCount, .showsPhysics]
-                    )
-                    .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight)
-                    .containerRelativeFrame(.horizontal, alignment: .center)
-                    .scrollTransition { content, phase in
-                        content
-                            .opacity(phase.isIdentity ? 1 : 0)
+        ZStack(alignment: .top) {
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 0) {
+                    ForEach(vm.skScenes, id: \.self) { scene in
+                        SpriteView(
+                            scene: scene,
+                            isPaused: false,
+                            preferredFramesPerSecond: 120,
+                            options: [.ignoresSiblingOrder, .shouldCullNonVisibleNodes],
+                            debugOptions: [.showsFPS, .showsNodeCount, .showsPhysics]
+                        )
+                        .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight)
+                        .containerRelativeFrame(.horizontal, alignment: .center)
+                        .scrollTransition { content, phase in
+                            content
+                                .opacity(phase.isIdentity ? 1 : 0)
+                        }
                     }
                 }
             }
-        }
-        .ignoresSafeArea()
-        .scrollTargetLayout()
-        .scrollTargetBehavior(.paging)
-        .scrollBounceBehavior(.basedOnSize)
-        .onAppear {
-          //  vm.resetVM()
-            vm.createGKScenes(with: levelTitle)
+            .simultaneousGesture(!isLocked ? DragGesture().onEnded({ _ in
+                isLocked = true
+            }) : nil)
+            .scrollDisabled(isLocked)
+            .ignoresSafeArea()
+            .scrollTargetLayout()
+            .scrollTargetBehavior(.paging)
+            .scrollBounceBehavior(.basedOnSize)
+            .onAppear {
+                vm.createGKScenes(with: levelTitle)
+            }
+            .onDisappear {
+                vm.resetVM()
+            }
+            
+            VStack {
+                HStack {
+                    Text("Hello World")
+                        .foregroundStyle(.white)
+                    Button {
+                        isLocked.toggle()
+                    } label: {
+                        Text("Unlock Screen")
+                    }
+                }
+            }
+            .padding(.top, 80)
         }
     }
 }
